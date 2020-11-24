016ff3dae249e52b2d0abe66a30c28687cd696aa
Zoltan Balogh <zoltan@bakter.hu>
2020-11-24 20:32:41 +0200

    Update forum.py

--- forum.py
+++ forum.py
@@ -9,8 +9,9 @@ import subprocess
 import re
 import argparse
 from argparse import RawTextHelpFormatter
+import sys
 
-chrome_driver_path = '[...]/chromedriver'
+chrome_driver_path = '/home/balogh/chromedriver'
 chrome_options = Options()
 chrome_options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36")
 chrome_options.add_argument('--headless')
@@ -32,12 +33,15 @@ options = parser.parse_args()
 
 with webdriver as driver:
     wait = WebDriverWait(driver, 1)
+    driver.maximize_window()
+
+
     driver.get(options.topic)
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(1)
     forward = True
     page=1
     while forward:
-        results = driver.find_elements_by_xpath('/html/body/div[2]/div/table/tbody/tr[2]/td[2]/table')
+        results = driver.find_elements_by_xpath('//*[@id="maintd"]/table')
         for result in results:
             nick = result.find_element_by_xpath('./tbody/tr[1]/td[1]/a[2]/strong').text
             title=result.find_element_by_xpath('./tbody/tr[1]/td[1]/span/a[2]')
@@ -50,11 +54,13 @@ with webdriver as driver:
             artictle=result.find_element_by_xpath('./tbody/tr[2]/td/div')
             print(artictle.get_attribute('innerHTML'))
         page += 1
-        navilink_table =  driver.find_element_by_xpath('/html/body/div[2]/div/table/tbody/tr[2]/td[2]/form[1]/table/tbody/tr/td[1]')
+        navilink_table =  driver.find_element_by_xpath('//*[@id="maintd"]/form[1]/table/tbody/tr/td[1]')
         navilinks = navilink_table.find_elements_by_xpath('./a')
+
         for navilink in navilinks:
+            print(navilink.text)
             if str(page) ==  navilink.text:
-               navilink.click()
+               driver.execute_script("arguments[0].click();", navilink)
                forward=True
                break
             if not navilink.text:
